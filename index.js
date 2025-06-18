92a67e7ebc14e837c3b2a223c28f067c4b121115
byrongonzalez14 <byrongonzalez14@gmail.com>
2025-06-18 11:24:19 -0500

    fix: conections api+bd production

--- index.js
+++ index.js
@@ -10,38 +10,38 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: 'localhost',
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: 5432,
+    user: process.env.DB_USER,
+    host: 'localhost',
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
 });
 
 app.post('/subscribe', async (req, res) => {
-  const { email } = req.body;
+    const { email } = req.body;
 
-  if (!email) return res.status(400).json({ message: 'Email requerido' });
+    if (!email) return res.status(400).json({ message: 'Email requerido' });
 
-  try {
-    await pool.query('INSERT INTO subscribers (email) VALUES ($1)', [email]);
-    res.status(200).json({ message: 'Correo registrado correctamente' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Error al registrar el correo' });
-  }
+    try {
+        await pool.query('INSERT INTO subscribers (email) VALUES ($1)', [email]);
+        res.status(200).json({ message: 'Correo registrado correctamente' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error al registrar el correo' });
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
 
 app.get('/subscribers', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM subscribers ORDER BY id DESC');
-    res.status(200).json(result.rows);
-  } catch (error) {
-    console.error('Error al obtener suscriptores:', error);
-    res.status(500).json({ message: 'Error al obtener suscriptores' });
-  }
+    try {
+        const result = await pool.query('SELECT * FROM subscribers ORDER BY id DESC');
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error('Error al obtener suscriptores:', error);
+        res.status(500).json({ message: 'Error al obtener suscriptores' });
+    }
 });
 
