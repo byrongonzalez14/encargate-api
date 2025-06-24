5f6717a71f9d59839de6b4416ca9cea7f2aea45a
byrongonzalez14 <byrongonzalez14@gmail.com>
2025-06-24 15:28:34 -0500

    fix: Hide app.get

--- index.js
+++ index.js
@@ -36,17 +36,17 @@ app.listen(port, () => {
     console.log(`Servidor escuchando en puerto:${port}`);
 });
 
-app.get('/subscribers', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM subscribers ORDER BY id DESC');
-    res.status(200).json(result.rows);
-  } catch (error) {
-    console.error('Error al obtener números:', error);
-    res.status(500).json({ message: 'Error al obtener números' });
-  }
-});
+// app.get('/subscribers', async (req, res) => {
+//   try {
+//     const result = await pool.query('SELECT * FROM subscribers ORDER BY id DESC');
+//     res.status(200).json(result.rows);
+//   } catch (error) {
+//     console.error('Error al obtener números:', error);
+//     res.status(500).json({ message: 'Error al obtener números' });
+//   }
+// });
 
-app.get('/', (req, res) => {
-  res.send('Encárgate API funcionando ✅');
-});
+// app.get('/', (req, res) => {
+//   res.send('Encárgate API funcionando ✅');
+// });
 
